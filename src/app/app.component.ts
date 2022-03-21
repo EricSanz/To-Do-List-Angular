d0b990ea73d7699e26625806332f33a112e3928e
Eric <eric_sanz_rodriguez@hotmail.com>
2022-03-21 17:15:50 +0100

    created selectedTask and added new info to tasksArray

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,8 +10,11 @@ export class AppComponent {
   title = 'Tasks App';
 
   tasksArray: Tasks[] = [
-    {id: 1, task: "Make Laura's birthday cake.", completed: false},
-    {id: 2, task: "Do the laundry.", completed: false},
-    {id: 3, task: "Wash the dishes", completed: false},
+    {id: 1, task: "Make Laura's birthday cake.", note: "Remember to buy especial chocolate for cover", date:"today", completed: false},
+    {id: 2, task: "Do the laundry.", note: "At noon, when it's cheaper", date:"today", completed: false},
+    {id: 3, task: "Wash the dishes", note: "", date:"today", completed: false},
   ]
+
+  selectedTask: Tasks = new Tasks();
+
 }
